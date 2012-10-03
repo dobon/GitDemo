2ca4e555ddaefc5de320c8721764ac383b70b6f0
Michael Hall <phil.bronstead@gmail.com>
2012-10-03 11:33:07 -0700

    babby's first commit

---
+++ src/ubc/cpsc410/gitdemo/MainClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ubc.cpsc410.gitdemo;
+
+/**
+ * @author miguelvaldez
+ *
+ */
+public class MainClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
